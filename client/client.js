7d322420b027eeb1eca6bcbfe54d3d5fbc35b420
ExXTreMe315 <benwet3602@gmail.com>
2025-01-30 22:14:51 +0100

    calculate max drawables

--- client/client.js
+++ client/client.js
@@ -1,5 +1,6 @@
 /// <reference types="@altv/types-client" />
 import * as alt from 'alt-client';
+import * as native from "natives";
 import * as NativeUI from './includes/NativeUI/NativeUi.js';
 
 let cloth = {
@@ -32,48 +33,66 @@ const clothMenuItems = {
 const clothMenu = new NativeUI.Menu("Clothes", "Clothes Menu", new NativeUI.Point(25, 25), "shopui_title_graphics_franklin", "shopui_title_graphics_franklin");
 const componentMenu = new NativeUI.Menu(capitalizeFirstLetter(clothMenuItems[cloth.component]), `${capitalizeFirstLetter(clothMenuItems[cloth.component])} Menu`, new NativeUI.Point(25, 25), "shopui_title_graphics_franklin", "shopui_title_graphics_franklin");
 
-const drawableItem = new NativeUI.UIMenuListItem("Drawable", "Set drawable", new NativeUI.ItemsCollection(createNumberArrayTill(500)), 1, { id: "drawable" })
-componentMenu.AddItem(drawableItem)
-
-const textureItem = new NativeUI.UIMenuListItem("Texture", "Set texture", new NativeUI.ItemsCollection(createNumberArrayTill(26)), 1, { id: "texture" })
-componentMenu.AddItem(textureItem)
-
-componentMenu.AddItem(new NativeUI.UIMenuItem("Apply", "Apply Cloth", { id: "apply" }))
-
 for (const [key, value] of Object.entries(clothMenuItems)) {
     const uiItem = new NativeUI.UIMenuItem(capitalizeFirstLetter(value), `Change ${capitalizeFirstLetter(value)}`, { id: value, componentID: key })
     clothMenu.AddItem(uiItem)
     clothMenu.AddSubMenu(componentMenu, uiItem)
 }
 
+clothMenu.ItemSelect.on((item) => {
+    if(!item.Data || !item.Data.componentID) return
+    cloth.component = parseInt(item.Data.componentID)
+
+    componentMenu.Title = capitalizeFirstLetter(clothMenuItems[cloth.component])
+    componentMenu.SubTitle = `Change ${capitalizeFirstLetter(clothMenuItems[cloth.component])}`
+
+    componentMenu.Clear()
+
+    let maxDrawableVariations = getMaxDrawableVariations(cloth.component)
+    let maxTextureVariations = 26
+    if(cloth.component >= 12) {
+        maxDrawableVariations = getMaxPropDrawableVariations(cloth.component - 12)
+    }
+    // @ToDo: Add maxTextureVariations for props (remember prop needs to refresh if drawable changes)
+
+    componentMenu.AddItem(
+        new NativeUI.UIMenuListItem(
+            "Drawable",
+            "Set drawable",
+            new NativeUI.ItemsCollection(createNumberArray(0, maxDrawableVariations)),
+            0,
+            { id: "drawable" }
+        )
+    )
+
+    componentMenu.AddItem(
+        new NativeUI.UIMenuListItem(
+            "Texture",
+            "Set texture",
+            new NativeUI.ItemsCollection(createNumberArray(0, maxTextureVariations)),
+            0,
+            { id: "texture" }
+        )
+    )
+
+    componentMenu.AddItem(new NativeUI.UIMenuItem("Apply", "Apply Cloth", { id: "apply" }))
+})
+
 componentMenu.ListChange.on((item, index) => {
     if(!item.Data.id) return
-    cloth[item.Data.id] = index - 1
-    alt.logDebug(cloth)
+    cloth[item.Data.id] = index
 })
 
 componentMenu.ItemSelect.on((item) => {
-    alt.logDebug(item.Data)
     if(!item.Data || !item.Data.id) return
-    alt.logDebug(cloth)
     if(cloth.component >= 12){
         alt.emitServer('setProp', cloth.component-12, cloth.drawable, cloth.texture)
     }
     alt.emitServer('setCloth', cloth.component, cloth.drawable, cloth.texture)
 })
 
-clothMenu.ItemSelect.on((item) => {
-    alt.logDebug(item.Data)
-    if(!item.Data || !item.Data.componentID) return
-    cloth.component = parseInt(item.Data.componentID)
-    componentMenu.Title = capitalizeFirstLetter(clothMenuItems[cloth.component])
-    componentMenu.SubTitle = `${capitalizeFirstLetter(clothMenuItems[cloth.component])} Menu`
-    drawableItem.Index = 0
-})
-
-function createNumberArrayTill(max) {
-    max--
-    let i = -1
+function createNumberArray(min, max) {
+    let i = min
     let array = []
     while (i <= max) {
         array.push(i)
@@ -84,6 +103,22 @@ function createNumberArrayTill(max) {
 
 function capitalizeFirstLetter(val) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
+function getMaxDrawableVariations(componentID){
+    return native.getNumberOfPedDrawableVariations(alt.Player.local.scriptID, componentID)
+}
+
+function getMaxTextureVariations(componentID, drawableID){
+    return native.getNumberOfPedTextureVariations(alt.Player.local.scriptID, componentID, drawableID)
+}
+
+function getMaxPropDrawableVariations(componentID){
+    return native.getNumberOfPedPropDrawableVariations(alt.Player.local.scriptID, componentID)
+}
+
+function getMaxPropTextureVariations(componentID, drawableID){
+    return native.getNumberOfPedPropTextureVariations(alt.Player.local.scriptID, componentID, drawableID)
 }
 
 alt.on('keyup', (key) => {
